ecd4588410ba2ca107aeece156b9211bc4da5446
Yash Sharma <139453096+zvbrsten@users.noreply.github.com>
2025-06-29 01:54:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import DownloadForm from "./components/downloadForm";
 function App() {
   return (
     <div style={{ maxWidth: "600px", margin: "auto", padding: "2rem" }}>
-      <h1>📤 Cross Device File Transfer</h1>
+      <h1> Cross Device File Transfer</h1>
       <UploadForm />
       <hr style={{ margin: "3rem 0" }} />
       <DownloadForm />
