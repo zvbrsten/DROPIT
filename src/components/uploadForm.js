3af4018d12306ecd5a78aba3b41c36eedcd05b80
Yash Sharma <139453096+zvbrsten@users.noreply.github.com>
2025-10-06 00:38:11 +0530

    Update uploadForm.js

--- src/components/uploadForm.js
+++ src/components/uploadForm.js
@@ -40,7 +40,7 @@ const UploadForm = () => {
       setIsUploading(true);
       setProgress(0);
       
-      const res = await axios.post("http://localhost:8000/api/upload", formData, {
+      const res = await axios.post("https://dropit-backend-three.vercel.app"/api/upload", formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
@@ -385,3 +385,4 @@ const UploadForm = () => {
 };
 
 export default UploadForm;
+
